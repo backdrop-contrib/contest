15c5241456218c724e961a157ea367cbe8264346
Bill Kelly <bkelly@genuineinteractive.com>
2015-07-07 23:44:00 -0400

    Removed superfluous contest entry. Added the contest list page. Renamed _contest_get_list_block_data() to _contest_get_list_data(<view_mode>) so it can be used in bpth the list page and block.

--- templates/contest-list-block.tpl.php
+++ templates/contest-list-block.tpl.php
@@ -1,3 +1,14 @@
+<?php
+
+/**
+ * @file contest-list-block.tpl.php
+ * Template for a contest's list block.
+ *
+ * Available variables:
+ * - $data (array)
+ * - - content (render array)
+ */
+?>
 <?php foreach ($data as $content): ?>
   <?php print render($content); ?>
 <?php endforeach; ?>
